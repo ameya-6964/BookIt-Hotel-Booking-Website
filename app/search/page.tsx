ec64bf2a8b68dcb79b44fdceea749533410c90d0
ameya-6964 <belvalkar7@gmail.com>
2023-11-05 02:10:07 +0530

    Implemented Search Functionality

---
+++ app/search/page.tsx
@@ -0,0 +1,14 @@
+import Search from "@/components/Search";
+import React from "react";
+export const metadata = {
+  title: "Serach Rooms",
+};
+const SearchPage = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default SearchPage;
