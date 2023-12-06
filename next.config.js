c1d449835502d7add6f5679fc68765e10faf374f
ameya-6964 <belvalkar7@gmail.com>
2023-12-07 01:12:12 +0530

    Added Authentication Logic

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     API_URL: "http://localhost:3000",
     DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-v2",
+    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_SECRET: "KSKSKSKDKASKDOASFDJIDIAOSDOASKDOASJC534534534534",
   },
   images: {
     remotePatterns: [
