e90401e6fe92d150f3a68184db1e93b3ff97bc72
ameya-6964 <belvalkar7@gmail.com>
2023-12-08 00:03:15 +0530

    Integrated Redux

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -2,11 +2,15 @@
 
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Toaster />
-      <SessionProvider>{children}</SessionProvider>
+      <Provider store={store}>
+        <SessionProvider>{children}</SessionProvider>
+      </Provider>
     </>
   );
 }
