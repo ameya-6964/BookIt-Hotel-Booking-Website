e3d06d4e59cdebef152bbc0e5260a6f2a438c2ec
ameya-6964 <belvalkar7@gmail.com>
2023-12-20 17:58:29 +0530

    Updated Seeder

--- seeder/data.ts
+++ seeder/data.ts
@@ -1,10 +1,10 @@
 export const rooms = [
   {
-    name: "Charming Studio < 10 Miles to Wells' Beaches!",
+    name: "Taj Cidade de Goa Heritage, Goa",
     pricePerNight: 1500,
     description:
       "A friendly atmosphere and natural delights await your visit to the town of Wells! Stay at this well-equipped 1-bath studio and enjoy easy access to several beaches, including Wells Beach and Drakes Island Beach, as well as Rachel Carson National Wildlife Refuge - the best spot for wildlife viewing just 8 miles away. Not to mention, with the downtown area just 10 minutes from vacation rental.",
-    address: "4667 Bicetown Street, New York, NY, 10004",
+    address: "Vainguinim Beach, Panaji, Goa 403004",
     guestCapacity: 1,
     numOfBeds: 1,
     isInternet: true,
@@ -28,11 +28,12 @@ export const rooms = [
     reviews: [],
   },
   {
-    name: "Picturesque 2-Story Farmhouse w/Private Hot Tub",
+    name: "Hard Rock Hotel Goa w/Private Hot Tub",
     pricePerNight: 2000,
     description:
       "Find plenty of space for a family or a large group at this picturesque Wells home, the perfect spot for a relaxing getaway in charming, quintessential Maine style! Spend your days on the beautiful nearby beaches, and come home to a large backyard and orchard where your kids can play, as well as a patio with a gas grill for barbecues on summer afternoons.",
-    address: "200 Olympic Dr, Stafford, VS, 22554",
+    address:
+      "370/14, Bishop Alex Dias Rd, Porba Vaddo, Bardez, Calangute, Saligao, Goa",
     guestCapacity: 2,
     numOfBeds: 2,
     isInternet: true,
@@ -56,11 +57,11 @@ export const rooms = [
     reviews: [],
   },
   {
-    name: "Downtown Portsmouth Private Getaway! Hot Tub",
+    name: "The Taj Mahal Palace, Mumbai",
     pricePerNight: 800,
     description:
       "Absolutely the best location in Portsmouth! Beautifully furnished, this spacious and private home is perfectly suited for taking in all of the must-see sights and historic landmarks that make this vibrant city so unique. Situated near the banks of the Piscataqua River just minutes away from Strawbery Banke Museum, Prescott Park, USS Albacore Museum, Market Square and more!",
-    address: "3747 Parkway Street, Apple Valley, CA, 92307",
+    address: "Apollo Bandar, Colaba, Mumbai, Maharashtra 400001",
     guestCapacity: 3,
     numOfBeds: 2,
     isInternet: true,
@@ -84,11 +85,12 @@ export const rooms = [
     reviews: [],
   },
   {
-    name: "Spacious Suite in a quiet Boston neighborhood.",
+    name: "Regent Hotel Taj Neighborhood.",
     pricePerNight: 1900,
     description:
       "Our largest room with a queen bed and foldable sofa. Comfortably fits 2-3 adults, four adults maximum. Decorated with Irish-themed colors and arts. It has a private bathroom with a shower, equipped with smart TV, mini-fridge, desk, chairs. Enjoy City views at the sitting area by the window. Located on the 3rd floor with no elevator.",
-    address: "224 Cherry St, Buffalo, NY, 14202",
+    address:
+      "The Taj Mahal Palace Hotel, 8, Best Road Behind, Off, Colaba, Mumbai, Maharashtra",
     guestCapacity: 6,
     numOfBeds: 3,
     isInternet: true,
@@ -116,11 +118,12 @@ export const rooms = [
     reviews: [],
   },
   {
-    name: "Shangri-La Hotel, Washington",
+    name: "The Golden Palms Hotel & Spa Bangalore",
     pricePerNight: 1000,
     description:
       "Our largest room with a queen bed and foldable sofa. Comfortably fits 2-3 adults, four adults maximum. Decorated with Irish-themed colors and arts. It has a private bathroom with a shower, equipped with smart TV, mini-fridge, desk, chairs. Enjoy City views at the sitting area by the window. Located on the 3rd floor with no elevator.",
-    address: "4724  Mudlick Road, Yakima, WA, 98902",
+    address:
+      "Golden Palms Avenue, Off, Tumkur Road, Nagrur Village, Dasanapura, Alur, Karnataka 562123",
     guestCapacity: 2,
     numOfBeds: 1,
     isInternet: true,
@@ -144,11 +147,12 @@ export const rooms = [
     reviews: [],
   },
   {
-    name: "Hotel Chimayo de Santa Fe Rooms",
+    name: "The Oterra Hotel",
     pricePerNight: 4000,
     description:
       "Our largest room with a queen bed and foldable sofa. Comfortably fits 2-3 adults, four adults maximum. Decorated with Irish-themed colors and arts. It has a private bathroom with a shower, equipped with smart TV, mini-fridge, desk, chairs. Enjoy City views at the sitting area by the window. Located on the 3rd floor with no elevator.",
-    address: "1029  Godfrey Street, Portland, OR, 97205",
+    address:
+      "43, Velankani Tech Park, Hosur Rd, Electronics City Phase 1, Electronic City, Bengaluru, Karnataka 5601005",
     guestCapacity: 2,
     numOfBeds: 2,
     isInternet: true,
@@ -168,11 +172,12 @@ export const rooms = [
     reviews: [],
   },
   {
-    name: "Hotel Garni Ischgl 4 star room comfort",
+    name: "Courtyard by Marriott Mahabaleshwar",
     pricePerNight: 3500,
     description:
       "Our largest room with a queen bed and foldable sofa. Comfortably fits 2-3 adults, four adults maximum. Decorated with Irish-themed colors and arts. It has a private bathroom with a shower, equipped with smart TV, mini-fridge, desk, chairs. Enjoy City views at the sitting area by the window. Located on the 3rd floor with no elevator.",
-    address: "2645  Parrish Avenue, SAINT LOUIS, MO, 63116",
+    address:
+      "19/B, Metgutad, Panchgani - Mahabaleshwar Rd, Mahabaleshwar, Maharashtra 412806",
     guestCapacity: 5,
     numOfBeds: 3,
     isInternet: true,
@@ -192,11 +197,12 @@ export const rooms = [
     reviews: [],
   },
   {
-    name: "The Spa at Base Camp at Kerry Hotel",
+    name: "Treat Resort Silvassa",
     pricePerNight: 4500,
     description:
       "Our largest room with a queen bed and foldable sofa. Comfortably fits 2-3 adults, four adults maximum. Decorated with Irish-themed colors and arts. It has a private bathroom with a shower, equipped with smart TV, mini-fridge, desk, chairs. Enjoy City views at the sitting area by the window. Located on the 3rd floor with no elevator.",
-    address: "3118  Red Hawk Road, Garfield, MN, 56332",
+    address:
+      "Treat Resort, Sayli Road, Silvassa, Dadra and Nagar Haveli and Daman and Diu 396230",
     guestCapacity: 2,
     numOfBeds: 1,
     isInternet: true,
